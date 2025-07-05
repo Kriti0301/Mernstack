b5102f07763eee7ea3c6f2d6574cce256e8c0123
Kriti Jodhani <kr422970@dal.ca>
2025-07-04 23:14:25 -0300

    Tutorial 6

--- backend/server.js
+++ backend/server.js
@@ -40,13 +40,11 @@ app.get("/", (req, res) => {
 
 
 
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+
+  mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
+
 
 
 // Start server
